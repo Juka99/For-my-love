242ad79c6675b1668ead185266c4a08a254b7cfe
Aleksa Jukic <yukajunior007@gmail.com>
2020-06-17 21:27:10 +0200

    Add files via upload

---
+++ full.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		autoScrolling:true,
+        scrollHorizontally: true,
+        scrollingSpeed : 1000,
+        controlArrows : false,
+        slidesNavigation : true,
+	});
+
+});
